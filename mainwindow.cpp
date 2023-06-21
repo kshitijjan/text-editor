8fd6feb2985b743666ef3f841551bd5e45fa1028
Kshitij Jain <kshitiz.jain636@gmail.com>
2023-06-21 22:13:29 +0530

    removed borders

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,6 +14,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
 
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
     isBold = false;
     isSuperscript = false;
     isSubscript = false;
